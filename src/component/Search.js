48445e3ca9847b48c12149d8b92e57ee40f07c80
yoni <yoni@un7qi3.co>
2019-10-15 20:19:29 +0900

    Revert "상단 네비바랑 검색창"
    This reverts commit 7e5b92ae567bea23f31ec74862982aa93ed2a28a.

--- src/component/Search.js
+++ src/component/Search.js
@@ -4,12 +4,7 @@ import '../css/search.css';
 function Search() {
   return (
     <div className="Search">
-      <form className="search-form">
-        <input type="text" name="userName" className="search-text" placeholder="소환사 이름"></input>
-        <button type="submit" className="search-btn">
-          <p>검색</p>
-        </button>
-      </form>
+      Hello Search! 
     </div>
   );
 }
--- src/component/Wheader.js
+++ src/component/Wheader.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import '../css/wheader.css';
-// import main_logo from '../images/main_logo.png'
 
 function Wheader() {
   return (
     <div className="wheader">
-      <div className="nav-bar">
-        <div className="nav-leftblank"></div>
-        <div className="nav-title">
-          <p>우리들의 내전 이야기</p>
-          <div className="logo"><img src="/images/main_logo.png" /></div>
-        </div>
-        <div className="members">
-          <p href="" className="sign-up">회원가입</p>
-          <p href="" className="sign-in">로그인</p>
-        </div>
-      </div>
+      Hello Header! 
     </div>
   );
 }
